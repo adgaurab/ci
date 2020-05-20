5d194c0962118de9ac6c9222888cae2333bca9ed
gaurav <adhikarigaurab999@outlook.com>
2020-05-20 16:58:19 +0930

    anish dai le sikako

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,21 +1,20 @@
 <?php
 class Admin extends My_Controller{
 	
-	
+	function __construct(){
+		parent::__construct();
+		 if(!$this->session->userdata('user_id')){
+			 return redirect('login');
+		 }
+	}
 	public function dashboard()
 	{
 		$this->load->model('articlesmodel',);
 		
 		$articles =$this->articlesmodel->articles_list();
-		
 		$this->load->view('admin/dashboard',['articles'=>$articles]);
-		print_r($this->session->userdata);
 	}
 	
-	//public function _construct(){
-		// parent::_construct();
-		// if(!$this->session->userdata('user_id'))
-		//	 return rediect('login');
 	
 		
 }
